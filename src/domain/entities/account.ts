b9416f82a266a2050917595eeac2b1d1062978ba
Flávio Arêas <vflavio9@gmail.com>
2023-11-11 11:06:51 -0300

    feat: garantindo que o CreateAccountController é chamado corretamente

--- src/domain/entities/account.ts
+++ src/domain/entities/account.ts
@@ -3,7 +3,7 @@ import { Entity } from '@core/domain/entity';
 export interface IAccount {
   email: string;
   password: string;
-  role: 'ADMIN' | 'USER';
+  role?: 'ADMIN' | 'USER';
 }
 
 export class Account extends Entity<IAccount> {
@@ -12,7 +12,14 @@ export class Account extends Entity<IAccount> {
   }
 
   static create(props: IAccount, id?: string) {
-    const account = new Account(props, id);
+    const account = new Account(
+      {
+        email: props.email,
+        password: props.password,
+        role: props.role ?? 'USER',
+      },
+      id
+    );
     return account;
   }
 }
